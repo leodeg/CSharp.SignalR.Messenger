5deb9ed03e49c033f2895109ed7c62dd551a9615
LeoDeg <leodeg.dev@gmail.com>
2020-10-14 11:17:24 +0300

    feat: created asp.net core application and a console application for testing SignalR "send message" method

---
+++ Messenger/Messenger.Client.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Messenger.Client.Console
+{
+	class Program
+	{
+		static HubConnection HubConnection;
+
+		static async Task Main(string[] args)
+		{
+			HubConnection = new HubConnectionBuilder()
+				.WithUrl("http://localhost:3744/notification")
+				.Build();
+
+			HubConnection.On<string>("Send", message => System.Console.WriteLine($"Message from server: {message}"));
+
+			await HubConnection.StartAsync();
+
+			bool isExit = false;
+			while (!isExit)
+			{
+				var message = System.Console.ReadLine();
+
+				if (message != "exit")
+					await HubConnection.SendAsync("SendMessage", message);
+				else isExit = true;
+			}
+
+			System.Console.WriteLine("Press any key...");
+			System.Console.ReadLine();
+		}
+	}
+}
---
+++ Messenger/Messenger/Hubs/NotificationHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messenger.Hubs
+{
+	public class NotificationHub : Hub
+	{
+		public Task SendMessage (string message)
+		{
+			return Clients.Others.SendAsync("Send", message);
+		}
+	}
+}
