7027334391b38eda7f8271e251b1abec811c4a94
LeoDeg <leodeg.dev@gmail.com>
2020-10-15 23:21:23 +0300

    feat: added web UI with a javascript example of the SignalR

--- Messenger/Messenger.Client.Web/Startup.cs
+++ Messenger/Messenger.Client.Web/Startup.cs
@@ -2,18 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Messenger.Server.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
-namespace Messenger.Server
+namespace Messenger.Client.Web
 {
 	public class Startup
 	{
@@ -27,19 +23,7 @@ namespace Messenger.Server
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddCors();
-			services.AddSignalR();
-
-			services.AddControllers();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo
-				{
-					Version = "v1",
-					Title = "Messenger API",
-					Description = "Messenger server for chat"
-				});
-			});
+			services.AddControllersWithViews();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,31 +33,24 @@ namespace Messenger.Server
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
-			app.UseCors(policy =>
+			else
 			{
-				policy
-					.SetIsOriginAllowed(origin => true)
-					.AllowAnyHeader()
-					.AllowAnyMethod()
-					.AllowCredentials();
-			});
-
-			app.UseStaticFiles();
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
 			app.UseRouting();
+
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapControllers();
-				endpoints.MapHub<MessageHub>("/messages");
-			});
-
-			app.UseSwagger();
-			app.UseSwaggerUI(options =>
-			{
-				options.SwaggerEndpoint("/swagger/v1/swagger.json", "Messenger Swagger Api");
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
 			});
 		}
 	}
